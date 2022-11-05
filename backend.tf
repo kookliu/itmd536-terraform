4160dbab2236b76e0f239031adcdc1b142c84223
LIUJIA161 <liujia161@huawei.com>
2022-11-06 07:09:58 +0800

    update files

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-application-account-leod0zvp"
+    bucket = "terraform-application-account-cdflqtld"
     key = "application-account/terraform.tfstate"
     region = "ap-southeast-1"
     dynamodb_table = "app-state"
