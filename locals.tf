801850c8aa550809c4a91874199cf9f9164b4f69
LIUJIA161 <liujia161@huawei.com>
2022-11-05 22:10:25 +0800

    init files

---
+++ locals.tf
@@ -0,0 +1,12 @@
+locals {
+  name   = "liujia"
+  region = "ap-southeast-1"
+  availability_zone = "${local.region}a"
+
+  
+  tags = {
+    Example    = local.name
+    GithubRepo = "terraform-aws-vpc"
+    GithubOrg  = "terraform-aws-modules"
+  }
+}
---
+++ vpc.tf
@@ -0,0 +1,24 @@
+module "vpc" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=v3.18.1"
+
+  name = local.name
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_ipv6 = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  enable_nat_gateway = false
+  single_nat_gateway = true
+
+
+  tags = local.tags
+
+  vpc_tags = {
+    Name = "vpc-liujia"
+  }
+}
