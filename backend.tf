d48ddf32f22c89876b2244c7f9c07b972a20fc7b
LIUJIA161 <liujia161@huawei.com>
2022-11-06 05:07:09 +0800

    latest

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-application-account-7cfqt3b0"
+    bucket = "terraform-application-account-leod0zvp"
     key = "application-account/terraform.tfstate"
     region = "ap-southeast-1"
     dynamodb_table = "app-state"
+    profile = "application"
   }
 }
